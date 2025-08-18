2c5d81e0b1a8df883dff1ee56c011d61dff18dff
Jeffrey Drummond <jeffdtgif@yahoo.com>
2025-08-17 21:53:21 -0400

    First half of section 13

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../../shared/models/product';
 import { MatCard } from "@angular/material/card";
 import { MatCardContent } from "@angular/material/card";
@@ -7,6 +7,7 @@ import { MatCardActions } from "@angular/material/card";
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-product-item',
@@ -24,4 +25,6 @@ import { RouterLink } from '@angular/router';
 })
 export class ProductItemComponent {
   @Input() product?: Product;
+  cartService = inject(CartService);
+  
 }
